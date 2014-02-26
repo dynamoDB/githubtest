04a2d7ea231ad4bd702bf0505e653981dbbfbaa2
leishi <leishi@hengtiansoft.com>
2014-02-26 14:42:20 +0800

    Update test.java

--- src/statestreet/test.java
+++ src/statestreet/test.java
@@ -4,5 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("hello word");
 		System.out.println("hello shilei");
+		System.out.println("what a beautifull day");
+		
 	}
 }
